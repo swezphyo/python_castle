9195f093a6867e0de5e1eb63b78b012feb407632
swezphyo <vinusrainbow@gmail.com>
2019-06-23 22:35:35 +0630

    started design the connect_sqlserver script

---
+++ python_chamber/connect_sqlserver.py
@@ -0,0 +1,13 @@
+"""A script to connect sql server from python using Pyodbc library
+- Includes basic SELECT , INSERT data into sql server
+"""
+import pyodbc
+import sys
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+	sys.exit(0)
