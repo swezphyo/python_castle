7d7a5281c34eb4c6ef4f4ef3c5aab3772456bdfc
swezinphyo <swezin.phyo@thitsaworks.com>
2019-11-04 11:31:09 +0630

    generate random numbers

---
+++ python_chamber/generate_random.py
@@ -0,0 +1,6 @@
+# range() - the number of rows you want to generate
+# using random.randint() - the number between to insert 
+import random
+for ran in range(0,30):
+    print("%0.9d" % random.randint(10,100))
+
