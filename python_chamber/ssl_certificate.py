8d6385f1c98400f3f0bf7a90508691c77dc3d749
Swe Zin Phyo <vinusrainbow@gmail.com>
2019-12-29 17:53:53 +0630

    update script by adding timestamp validation

--- python_chamber/ssl_certificate.py
+++ python_chamber/ssl_certificate.py
@@ -1,9 +1,20 @@
 import ssl, socket
+from datetime import datetime
 
-hostname = ['yomaland.com','starcityyangon.com','yomalife.com']
+hostname = ['yomaland.com','starcityyangon.com']
 for host in hostname: 
+	print(host) #print domain name for debugging 
+
 	ctx = ssl.create_default_context()
 	s = ctx.wrap_socket(socket.socket(), server_hostname=host)
 	s.connect((host, 443))
 	cert = s.getpeercert()
-	print(cert)
+
+	#date_range to get ssl expired date - notAfter
+	date_range = cert.get('notAfter')
+
+	#ssl.cert_time_to_seconds for get cert_time in epoch
+	timestamp = ssl.cert_time_to_seconds(date_range)
+	#convert epoch time to utc format to validate
+	time_utc = datetime.utcfromtimestamp(timestamp)
+	print(time_utc)
