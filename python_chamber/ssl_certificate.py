449d39f7e12f6d48123c35ca89d66e2b82819dca
Swe Zin Phyo <vinusrainbow@gmail.com>
2019-12-30 14:16:53 +0630

    added timedelta to get expire days is within 30 days or not

--- python_chamber/ssl_certificate.py
+++ python_chamber/ssl_certificate.py
@@ -1,7 +1,7 @@
 import ssl, socket
 from datetime import datetime
 
-hostname = ['yomaland.com','starcityyangon.com']
+hostname = ['yomaland.com','starcityyangon.com','balloonsoverbaganbookings.com']
 for host in hostname: 
 	print(host) #print domain name for debugging 
 
@@ -9,6 +9,7 @@ for host in hostname:
 	s = ctx.wrap_socket(socket.socket(), server_hostname=host)
 	s.connect((host, 443))
 	cert = s.getpeercert()
+	#print(cert)
 
 	#date_range to get ssl expired date - notAfter
 	date_range = cert.get('notAfter')
@@ -18,3 +19,13 @@ for host in hostname:
 	#convert epoch time to utc format to validate
 	time_utc = datetime.utcfromtimestamp(timestamp)
 	print(time_utc)
+	datetime_now = datetime.now()
+	expire = time_utc - datetime_now #expire is timedelta object
+
+	#use (timedelta.days) to get only days from timedelta object
+	expire_days = expire.days
+
+	if expire_days <= 30:
+		print ('Expired within {0} days'.format(expire_days))
+	else:
+		print('Need long time to expire')
